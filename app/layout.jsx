2d20fe36a23bfb9e2101f44b76486d0a05598e94
scicluna <88948700+scicluna@users.noreply.github.com>
2023-05-17 20:00:31 -0400

    user Provider Context start

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,13 +11,15 @@ const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
             <body>
-                <div className='main'>
-                    <div className='gradient' />
-                </div>
-                <main className='relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6'>
-                    <Nav />
-                    {children}
-                </main>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+                    <main className='relative z-10 flex justify-center items-center flex-col max-w-7xl mx-auto sm:px-16 px-6'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
             </body>
         </html>
     )
